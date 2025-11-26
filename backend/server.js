05317bd65ee1e80a56843a38eb5012ba144305de
vedavyaskv <vedavyaskv05@gmail.com>
2025-11-27 00:10:32 +0530

    Update

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors({
   origin: [
-    "https://globetrekker-travel-website.vercel.app", 
+    "https://globetrekker-travel-website.vercel.app",
+    /\.vercel\.app$/,
     "http://localhost:3000",
     "http://localhost:5173",
   ],
